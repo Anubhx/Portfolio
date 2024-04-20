c8faa80bd39c78d00d19228520e4bd1b533e84a6
Anubhav Raj <anubhav.raj.srivastav27@gmail.com>
2024-04-20 23:24:13 +0530

    Update App.js to include styled-components and add new components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
-
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme } from './utils/Themes';
+import Navbar from './components/Navbar';
+import Hero from './components/HeroSection';
+import Skills from './components/Skills';
+import Education from './components/Education';
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+`;
+const Wrapper = styled.div`
+background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), 
+linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+width: 100%;
+clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+`;
 function App() {
   return (
-    <div className="App">
-      Portfolio
-    </div>
+   <ThemeProvider theme={darkTheme}>
+    <Navbar/>
+    <Body>
+      <Hero/>
+      <Wrapper>
+        <Skills/>
+        <Education/>
+      </Wrapper>
+    </Body>
+   </ThemeProvider>
   );
 }
 export default App;
