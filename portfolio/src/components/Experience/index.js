94776c4108f574f7db5a95ccb9b747e204288793
Anubhav Raj <anubhav.raj.srivastav27@gmail.com>
2024-04-22 16:16:52 +0530

    Update default.js theme and App.js component

--- portfolio/src/components/Experience/index.js
+++ portfolio/src/components/Experience/index.js
@@ -7,8 +7,8 @@ import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineDot from '@mui/lab/TimelineDot';
-import { education, experiences } from '../../data/constants';
-import EducationCard from '../Cards/EducationCard';
+import ExperienceCard from '../Cards/ExperienceCard';
+import { experiences } from '../../data/constants';
 
 const Container = styled.div`
     display: flex;
@@ -17,7 +17,7 @@ const Container = styled.div`
     position: relative;
     z-index: 1;
     align-items: center;
-    padding: 0px 0px 60px 0px;
+    padding: 40px 0px 80px 0px;
     @media (max-width: 960px) {
         padding: 0px;
     }
@@ -31,7 +31,7 @@ const Wrapper = styled.div`
     flex-direction: column;
     width: 100%;
     max-width: 1350px;
-    padding: 40px 0px 0px 0px;
+    padding: 80px 0;
     gap: 12px;
     @media (max-width: 960px) {
         flex-direction: column;
@@ -70,32 +70,29 @@ const TimelineSection = styled.div`
     align-items: center;
     justify-content: center;
     gap: 12px;
-    @media (max-width: 660px) {
-        align-items: end;
-    }
 `;
 
 
 
-const index = () => {
+const Experience = () => {
     return (
-        <Container id="education">
+        <Container id="experience">
             <Wrapper>
-                <Title>Education</Title>
+                <Title>Experience</Title>
                 <Desc>
-                    My education has been a journey of self-discovery and growth. My educational details are as follows.
+                    My work experience as a software engineer and working on different companies and projects.
                 </Desc>
                 <TimelineSection>
                     <Timeline>
-                        {education.map((education,index) => (
-                            <TimelineItem >
-                                <TimelineContent sx={{ py: '12px', px: 2 }}>
-                                    <EducationCard education={education}/>
-                                </TimelineContent>
+                        {experiences.map((experience,index) => (
+                            <TimelineItem>
                                 <TimelineSeparator>
                                     <TimelineDot variant="outlined" color="secondary" />
-                                    {index !== experiences.length  && <TimelineConnector style={{ background: '#854CE6' }} />}
+                                    {index !== experiences.length - 1 && <TimelineConnector style={{ background: '#854CE6' }} />}
                                 </TimelineSeparator>
+                                <TimelineContent sx={{ py: '12px', px: 2 }}>
+                                    <ExperienceCard experience={experience}/>
+                                </TimelineContent>
                             </TimelineItem>
                         ))}
                     </Timeline>
@@ -106,4 +103,4 @@ const index = () => {
     )
 }
 
-export default index
+export default Experience
--- portfolio/src/data/constants.js
+++ portfolio/src/data/constants.js
@@ -1,3 +1,29 @@
+
+import uem_logo from '../images/uem_logo.png';
+import school1 from '../images/school1.png';
+import school2 from '../images/school2.png';
+import HeroImg from '../images/HeroImage.jpg';
+
+
+  /**
+   * Array of project objects.
+   * @typedef {Object[]} Project
+   * @property {number} id - The unique identifier of the project.
+   * @property {string} title - The title of the project.
+   * @property {string} date - The duration of the project.
+   * @property {string} description - The description of the project.
+   * @property {string} image - The URL of the project image.
+   * @property {string[]} tags - The tags associated with the project.
+   * @property {string} category - The category of the project.
+   * @property {string} github - The URL of the project's GitHub repository.
+   * @property {string} webapp - The URL of the project's web application.
+   * @property {Object[]} member - The members associated with the project.
+   * @property {string} member.name - The name of the member.
+   * @property {string} member.img - The URL of the member's image.
+   * @property {string} member.linkedin - The URL of the member's LinkedIn profile.
+   * @property {string} member.github - The URL of the member's GitHub profile.
+   */
+
 export const Bio = {
     name: "Anubhav",
     roles: [
@@ -119,8 +145,8 @@ export const Bio = {
         },
         {
           name: "Miro",
-          // image: "<svg xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"100\" height=\"100\" viewBox=\"0 0 64 64\">",
-          image: "https://di8m9w6rqrh5d.cloudfront.net/JIlYx8qbpWSpOvsR2zvaK74zfLDaj0JO/800_68a36392-5b65-4b72-b6b5-4024b89160ad.png",
+           // image: "<svg xmlns=\"http://www.w3.org/2000/svg\" x=\"0px\" y=\"0px\" width=\"100\" height=\"100\" viewBox=\"0 0 64 64\">",
+           image: "https://di8m9w6rqrh5d.cloudfront.net/JIlYx8qbpWSpOvsR2zvaK74zfLDaj0JO/800_68a36392-5b65-4b72-b6b5-4024b89160ad.png",
         },
    
       ],
@@ -204,8 +230,8 @@ export const Bio = {
   export const education = [
     {
       id: 0,
-      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
-      school: "University Of Engineering & Management (UEM), Kolkata",
+      img: uem_logo,
+        school: "University Of Engineering & Management (UEM), Kolkata",
       date: "Oct 2021 - Sep 2025",
       grade: "8.1 CGPA",
       desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at University Of Engineering & Management (UEM), Kolkata. I have completed 4 semesters and have a CGPA of 8.1. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. ",
@@ -213,7 +239,7 @@ export const Bio = {
     },
     {
       id: 1,
-      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+      img: school1,
       school: "S.S. Govt. School, Muzzafarpur",
       date: "Apr 2019 - Apr 2021",
       grade: "70.2%",
@@ -222,7 +248,7 @@ export const Bio = {
     },
     {
       id: 2,
-      img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+      img: school2,
       school: "Asian School, Muzzafarpur",
       date: "Apr 2017 - Apr 2019",
       grade: "75.3%",
@@ -231,196 +257,87 @@ export const Bio = {
     },
   ];
   
+
   export const projects = [
     {
       id: 9,
-      title: "Trackify",
-      date: "Jun 2023 - Jul 2023",
+      title: "React Native AirBnB Clone with Clerk",
+      date: "April 2024 - Jul 2024",
       description:
-        "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
+      "The React Native AirBnB Clone with Clerk is a mobile app that mimics AirBnB's core features using React Native and Expo, integrating Clerk for authentication and including OAuth, mapping, and advanced UI components. It offers a practical example for building sophisticated mobile apps.",
       image:
-        "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+        "https://github.com/Anubhx/AirBnB-Clone-using-React-Native-and-Expo-/blob/main/screenshots/PIC01.png?raw=true",
       tags: [
-        "Docker",
-        "AWS",
-        "DuckDNS",
-        "Eslint",
-        "Husky",
-        "CI/CD",
-        "React Js",
-        "MongoDb",
-        "Node Js",
-        "Express Js",
-        "Redux",
+        "Android",
+        "typescript",
+        "React Native",
+        "Expo",
+        "Clerk",
       ],
-      category: "web app",
-      github: "https://github.com/Anubhx/Trackify",
-      webapp: "https://trackify.duckdns.org",
+      category: "android app",
+      github: "https://github.com/Anubhx/AirBnB-Clone-using-React-Native-and-Expo-",
+     // webapp: "https://trackify.duckdns.org",
     },
     {
       id: 0,
-      title: "Podstream",
+      title: "Language Talk ( Language learning App ) Case Study",
       date: "Apr 2023 - May 2023",
       description:
-        "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
-      image:
-        "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
-      tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
-      category: "web app",
-      github: "https://github.com/Anubhx/Podstream",
-      webapp: "https://podstream.netlify.app/",
+       "I conceptualized LanguageTalk, a user-friendly language learning app equipped with AI-driven features for grammar and pronunciation correction, showcasing my proficiency in UX/UI design. For the development of this project, I utilized design tools such as Figma and FigJam, and managed the workflow and documentation using Notion, demonstrating both my design expertise and project management skills.",
+           image:"https://anubhx.github.io/language_talk.png",
+        tags: [
+          "Figma",
+          "Miro",
+          "UI/UX Design",
+          "Wireframing",
+        ],      
+        category: "design",
+      github: "https://www.behance.net/gallery/167171313/Language-Talk-(-Language-learning-App-)-Case-Study",
+      webapp: "https://www.behance.net/gallery/167171313/Language-Talk-(-Language-learning-App-)-Case-Study",
       member: [
         {
           name: "Anubhav Raj",
-          img: "https://avatars.githubusercontent.com/u/64485885?v=4",
-          linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+          img: HeroImg,
+          linkedin: "https://www.linkedin.com/in/anubhax/",
           github: "https://github.com/Anubhx/",
-        },
-        {
-          name: "Upasana Chaudhuri",
-          img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-          linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-          github: "https://github.com/upasana0710",
         },
       ],
     },
     {
       id: 1,
-      title: "Vexa",
-      date: "Oct 2022 - Present",
+      title: "VirtuStore -An AR shopping app (UI/UX CASE STUDY)",
+      date: "Oct 2023 - Oct- 2023",
       description:
-        "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
+      "I designed VirtuStore, a mobile application that addresses the challenge of limited access to physical stores by enabling users to conveniently shop from home using their mobile devices. The app features an augmented reality (AR) visualization tool that allows for realistic viewing of products, aiding users in making informed purchase decisions. Additionally, I developed an in-store check feature utilizing AR technology, which allows users to virtually navigate and explore the entire store, significantly enhancing the shopping experience.",
       image:
-        "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+        "https://mir-s3-cdn-cf.behance.net/projects/404/68fabb168030969.Y3JvcCw0MzIwLDMzNzksMCww.png",
       tags: [
-        "React Js",
-        "MongoDb",
-        "Node Js",
-        "Express Js",
-        "Redux",
-        "NodeMailer",
+        "Figma",
+        "Miro",
+        "UI/UX Design",
+        "Wireframing",
       ],
-      category: "web app",
-      github: "https://github.com/Anubhx/Project-Management-App",
-      webapp: "https://vexa-app.netlify.app/",
+      category: "design",
+      github: "https://www.behance.net/gallery/167171313/Language-Talk-(-Language-learning-App-)-Case-Study",
+      webapp: "https://www.behance.net/gallery/167171313/Language-Talk-(-Language-learning-App-)-Case-Study",
     },
     {
       id: 2,
-      title: "Brain Tumor Detection",
-      date: "Jan 2023 - Mar 2023",
+      title: "EmployeePro-Workforce-Management-System",
+      date: "Nov 2023 - Nov 2023",
       description:
-        "Preprocessed and augmented the dataset to improve model accuracy, trained the model, created API using model and Python, and used React web app for the project's front end. Achievements: Achieved an accuracy of 99.2% to accurately detect brain tumors from medical images.",
+      "Employee Pro is a Workforce Management System that I developed using Java, Swing, and MySQL, designed to enhance organizational efficiency. The system features a user-friendly interface with intuitive design for easy navigation and data entry. It includes dynamic employee ID generation for automatic assignment of unique identifiers, facilitating efficient employee tracking. Additionally, the system offers seamless database connectivity with MySQL, ensuring robust data management and operational reliability.",
       image:
-        "https://github.com/Anubhx/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
-      tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
-      category: "machine learning",
-      github: "https://github.com/Anubhx/Brain-Tumor-Detection",
-      webapp: "https://brain-tumor.netlify.app/",
-      member: [
-        {
-          name: "Rishav Chanda",
-          img: "https://avatars.githubusercontent.com/u/64485885?v=4",
-          linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-          github: "https://github.com/Anubhx/",
-        },
-        {
-          name: "Upasana Chaudhuri",
-          img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-          linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-          github: "https://github.com/upasana0710",
-        },
+        "https://private-user-images.githubusercontent.com/80146839/283999865-9c0fe86f-b8e4-4a17-8eec-8d0578a7bcfe.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTM3ODE3MDEsIm5iZiI6MTcxMzc4MTQwMSwicGF0aCI6Ii84MDE0NjgzOS8yODM5OTk4NjUtOWMwZmU4NmYtYjhlNC00YTE3LThlZWMtOGQwNTc4YTdiY2ZlLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA0MjIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNDIyVDEwMjMyMVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTI3ZjI5NGFjZjhkZmI0OWU3MzZkY2QxOTkwOGYwMjlkMWYxMzhlMDQ3NjMxZDZkMWZjNjQyYWRjZDAxZDQ4YTYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.2xx9UypDSWr_Y8WDYw988v84ghfDwRo6a0YCSVDGyiw",
+      tags: [
+        "Java",
+        "MySQL",
+        "Swing",
+        "Database Management",
       ],
-    },
-    {
-      id: 3,
-      title: "Buckoid",
-      date: "Dec 2021 - Apr 2022",
-      description:
-        "App Is Currently In Playstore 100+ Downloads. This Project proposes an “Expense Tracking App”. Keep track of your personal expenses and compare them to your monthly income with the budget planner. It has Google Drive Cloud API for Backup of User Room Database. Made with Kotlin in MVVM Architecture & Live Data.",
-      image:
-        "https://camo.githubusercontent.com/fbf405040ae86b5d4a40f24e4ac034982cb8c0e2d850560ba64527997b258be8/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f6c6f67696e2d65613565322e61707073706f742e636f6d2f6f2f4255434b4f49442532302831292e706e673f616c743d6d6564696126746f6b656e3d32653735376235372d323964372d346263612d613562322d653164346538313432373435",
-      tags: ["Kotlin", "MVVM", "Room Database", "Google Drive Cloud API"],
-      category: "android app",
-      github: "https://github.com/Anubhx/Buckoid-Android-App",
-      webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
-    },
-    {
-      id: 10,
-      title: "Job Finding App",
-      date: "Jun 2023 - Jul 2023",
-      description:
-        "A Job Finding App made with React Native, Axios. Users can search for any job coming from API and apply there.",
-      image:
-        "https://user-images.githubusercontent.com/64485885/255237090-cf798a2c-1b41-4bb7-b904-b5353a1f08e8.png",
-      tags: ["React Native", "JavaScript", "Axios"],
-      category: "android app",
-      github: "https://github.com/Anubhx/Job-finder-App",
-      webapp: "https://github.com/Anubhx/Job-finder-App",
-    },
-    {
-      id: 4,
-      title: "Whatsapp Clone",
-      date: "Jul 2021",
-      description:
-        "A WhatsApp clone made with React JS, Firebase, and Material UI. It has Phone Authentication, Real-time Database. It has a chat room where users can chat with each other. It has a sidebar where users can see all the chat rooms and can create a new chat room. It has a login page where users can log in with their Google account.",
-      image:
-        "https://firebasestorage.googleapis.com/v0/b/whatsapp-clone-rishav.appspot.com/o/Screenshot%20(151).png?alt=media&token=48391593-1ef0-4a8c-a92a-eb82bdf38e89",
-      tags: ["React Js", "Firebase", "Firestore", "Node JS"],
-      category: "web app",
-      github: "https://github.com/Anubhx/Whatsapp-Clone-React-Js",
-      webapp: "https://whatsapp-clone-rishav.web.app",
-    },
-    {
-      id: 5,
-      title: "Todo Web App",
-      date: "Jun 2021",
-      description:
-        " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
-      image:
-        "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
-      tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
-      category: "web app",
-      github: "https://github.com/Anubhx/Todo-Web-App",
-      webapp: "https://rishav-react-todo.netlify.app/",
-    },
-    {
-      id: 6,
-      title: "Breaking Bad",
-      date: "Jun 2021",
-      description:
-        "A simple react app that shows the characters of the famous TV series Breaking Bad. It uses the Breaking Bad API to fetch the data. It also has a search bar to search for a particular character.",
-      image:
-        "https://camo.githubusercontent.com/cd07010cbeb90cb1b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
-      tags: ["React Js", "API", "Axios", "Node JS"],
-      category: "web app",
-      github: "https://github.com/Anubhx/Breaking-Bad",
-      webapp: "https://breaking-bad-webapp.netlify.app",
-    },
-    {
-      id: 7,
-      title: "Quiz App",
-      date: "Dec 2020 - Jan 2021",
-      description:
-        "A android quiz app made with Java and Firebase. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the quiz categories and can create a new quiz. It has a leaderboard where users can see the top 10 scorers. It has a search bar where users can search for a particular quiz.",
-      image:
-        "https://github-production-user-asset-6210df.s3.amazonaws.com/64485885/239726262-c1b061d1-d9d0-42ef-9f1c-0412d14bc4f6.gif",
-      tags: ["Java", "Android Studio", "Firebase", "Google Auth"],
-      category: "android app",
-      github: "https://github.com/Anubhx/Quiz-Earn",
-      webapp: "https://github.com/Anubhx/Quiz-Earn",
-    },
-    {
-      id: 8,
-      title: "Face Recognition",
-      date: "Jan 2021",
-      description:
-        "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
-      image:
-        "https://camo.githubusercontent.com/a8b3e1ebf26d4c95f75bc6671189a3590eb67eb8332d7c7452492990e731efb9/68747470733a2f2f77617265686f7573652d63616d6f2e696e67726573732e636d68312e707366686f737465642e6f72672f356137383335396561323762643261633232336437656662306639303831306437373930383436312f363837343734373037333361326632663633366336663735363432653637363937343638373536323735373336353732363336663665373436353665373432653633366636643266363137333733363537343733326633383339333633363339333232663332333433343333333033333339333832663333333636363330363533333636333032643331333336333632326433313331363533373264333833323335333832643334363433303633333936333635333136353334333133393265363736393636",
-      tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
-      category: "machine learning",
-      github: "https://github.com/Anubhx/Face-Recodnition-AI-with-Python",
-      webapp: "https://github.com/Anubhx/Face-Recodnition-AI-with-Python",
+      category: "Web App",
+      github: "https://github.com/Anubhx/-EmployeeManagementSystem/tree/master",
+      webapp: "https://github.com/Anubhx/-EmployeeManagementSystem/tree/master",
     },
   ];
   
