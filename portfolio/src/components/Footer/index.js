106f31d5bc36342c7b7e982057cdb467dae63d4b
Anubhav Raj <anubhav.raj.srivastav27@gmail.com>
2024-04-22 17:09:11 +0530

    Update Footer component with new name and fix navigation links

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -88,13 +88,13 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Rishav Chanda</Logo>
+        <Logo>Anubhav Raj</Logo>
         <Nav>
-          <NavLink href="#about">About</NavLink>
-          <NavLink href="#skills">Skills</NavLink>
-          <NavLink href="#experience">Experience</NavLink>
-          <NavLink href="#projects">Projects</NavLink>
-          <NavLink href="#education">Education</NavLink>
+        <NavLink href= "#about">About</NavLink>
+        <NavLink href= "#skills">Skills</NavLink>
+        <NavLink href= "#experience">Experiences</NavLink>
+        <NavLink href= "#projects">Projects</NavLink>
+        <NavLink href= "#education">Education</NavLink>
         </Nav>
         <SocialMediaIcons>
           <SocialMediaIcon href={Bio.facebook} target="display"><FacebookIcon /></SocialMediaIcon>
@@ -103,7 +103,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2023 Rishav Chanda. All rights reserved.
+          &copy; 2025 Anubhav Raj. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
