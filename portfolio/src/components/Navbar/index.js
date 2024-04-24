010d7ce48132786bb93c7c56f674228bbff187d3
Anubhav Raj <anubhav.raj.srivastav27@gmail.com>
2024-04-24 13:01:59 +0530

    Update name in Navbar component

--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -229,7 +229,7 @@ const Navbar = () => {
     <NavContainer>
     <NavLogo to='/'>
                 <a href="/" style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
-                    <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+                    <DiCssdeck size="3rem" /> <Span>Anubhav Raj</Span>
                 </a>
             </NavLogo>
         <MobileIcon>
